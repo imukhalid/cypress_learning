16313cbeba96e26f161bab867fe65ebd110ae746
Usama Khalid <usamakhalid889@gmail.com>
2024-05-13 00:52:50 +0500

    login_updated

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,20 @@
 describe('Login Test', () => {
   it('should login successfully with correct credentials', () => {
     // Visit the login page
-    cy.visit('/login');
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
     // Enter username and password
-    cy.get('input[name="username"]').type('your_username');
-    cy.get('input[name="password"]').type('your_password');
+    cy.get("input[name='username']").type('Admin');
+    cy.get("input[name='username']").should('have.value', 'Admin');
+
+    cy.get("input[name='password']").type('admin123');
+    cy.get("input[name='password']").should('have.value', 'admin123');
 
     // Click on the login button
-    cy.get('button[type="submit"]').click();
+    cy.get("button[type='submit']").should('be.visible').click();
 
     // Assertion to check if login was successful
     cy.url().should('include', '/dashboard');
-    cy.contains('Welcome').should('be.visible');
+    cy.contains('Dashboard').should('be.visible');
   });
 })
