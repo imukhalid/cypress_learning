3daf82c8b6b39f54576d6fdad5b58ab2b78de953
Usama Khalid <usamakhalid889@gmail.com>
2024-05-30 00:43:35 +0500

    hooks

--- cypress/e2e/Hooks&Tags.cy.js
+++ cypress/e2e/Hooks&Tags.cy.js
@@ -1,3 +1,7 @@
+// What are cypress hooks ? 
+// Cypress Hooks are used to carry out the certain operations prior/post every/each test.Some of the common hooks are as follows
+// In Cypress, a hook is a function that allows you to run specific code before or after certain actions or groups of actions in your test suite. Hooks are commonly used to set up preconditions or clean up after tests, ensuring that each test runs in a consistent and isolated environment.
+
 // before − It is executed, once the prior execution of any tests within a described block is carried out.
 // after − It is executed, once the post-execution of all the tests within a described block is carried out.
 // beforeEach − It is executed prior to the execution of an individual, it blocks within a described block.
@@ -7,3 +11,10 @@
 // Close App ---- after
 // Login ---- beforeEach
 // Logout ---- afterEach
+
+// What are Tags?
+// Cypress has 2 tags .only and .skip.
+
+// The .only tag is utilized to execute it block to which it is tagged
+
+// The .skip tag is utilized to exclude it block to which it is tagged.
